8a0a862c1b50ae3af19e210182efdf6713257db3
mmmldy <melody.rojano@outlook.com>
2025-10-05 22:09:44 +1000

    Update details.py

--- A2 starter/website/details.py
+++ A2 starter/website/details.py
@@ -1,17 +1,23 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Event, Comment
-from .forms import CommentForm
+from .forms import OrderForm, CommentForm
 
 detailsbp = Blueprint('details', __name__, url_prefix='/details')
 
-@detailsbp.route('/<id>/comment', methods = ['GET', 'POST'])
-def comment(id):
-  # here the form is created  form = CommentForm()
-  form = CommentForm()
-  if form.validate_on_submit():	#this is true only in case of POST method
-    print(f"The following comment has been posted: {form.text.data}")
-  # notice the signature of url_for
-  return redirect(url_for('details.show', id=1))
+@detailsbp.route("/")
+def index():
+    return render_template('order_details.html')
 
-def get_event():
-    
+@detailsbp.route('/event-details', methods=['GET', 'POST'])
+def order():
+    form = OrderForm()
+    return render_template('details.html', form=form)
+
+#@detailsbp.route('/<id>/comment', methods = ['GET', 'POST'])
+#def comment(id):
+  # form created for comment section
+  #form = CommentForm()
+  #if form.validate_on_submit():	#this is true only in case of POST method
+    #print(f"The following comment has been posted: {form.text.data}")
+  #sreturn redirect(url_for('details.show', id=id))
+
