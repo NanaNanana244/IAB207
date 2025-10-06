24e56e5f371fd5f800b18bed7918752d12de9329
May010111 <ath-ins-app@outlook.com>
2025-10-06 22:30:39 +1000

    log in page implemented

--- A2 starter/website/auth.py
+++ A2 starter/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, render_template, request, url_for, redirect
+from flask import Blueprint, flash, render_template, request, url_for, redirect, session
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
 from .models import User
@@ -10,30 +10,27 @@ auth_bp = Blueprint('auth', __name__)
 
 # this is a hint for a login function
 @auth_bp.route('/login', methods=['GET', 'POST'])
-# view function
 def login():
     login_form = LoginForm()
     error = None
     if login_form.validate_on_submit():
         user_name = login_form.user_name.data
         password = login_form.password.data
-        user = db.session.scalar(db.select(User).where(User.name==user_name))
+        user = db.session.scalar(db.select(User).where(User.username==user_name))
         if user is None:
             error = 'Incorrect user name'
-        elif not check_password_hash(user.password_hash, password): # takes the hash and cleartext password
+        elif not check_password_hash(user.password_hash, password):
             error = 'Incorrect password'
         if error is None:
             login_user(user)
-            nextp = request.args.get('next') # this gives the url from where the login page was accessed
+            nextp = request.args.get('next')
             print(nextp)
-            if next is None or not nextp.startswith('/'):
-                return redirect(url_for('index'))
+            if nextp is None or not nextp.startswith('/'):
+                return redirect(url_for('main.index'))
             return redirect(nextp)
         else:
             flash(error)
-
-    return render_template('user.html', form=login_form, heading='Login')
-
+    return render_template('login.html', form=login_form, heading='Login')
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
