1b95854eb84b40712d0c94bb4efb3ebb274f9d46
its-kayla <kphack24@gmail.com>
2025-10-12 13:45:21 +1000

    Update forms.py date validation stuff

--- A2 starter/website/forms.py
+++ A2 starter/website/forms.py
@@ -25,19 +25,18 @@ class RegisterForm(FlaskForm):
 
 #event creation
 class CreateEvent(FlaskForm):
+    def validate_eventdate(form, field):
+        print("Validating date:", field.data)
+        if field.data < date.today():
+            raise ValidationError('The date cannot be in the past.')
     title = StringField('Enter event name', [InputRequired()])
     artist = StringField('Enter artist/group name', [InputRequired()])
     image = FileField('Event Image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
-    date = DateField('Enter the date of event', [InputRequired()])
+    eventdate = DateField('Enter the date of event', validators=[InputRequired(), validate_eventdate])
     startTime = TimeField('Enter the starting time of event', [InputRequired()])
     location = StringField('Enter venue name', [InputRequired()])
     country = StringField('Enter country (Spelling must be correct)', [InputRequired()]) 
-    status = SelectField('Event Status', 
-                choices=[('Available', 'Available'), 
-                        ('Sold out', 'Sold out'),
-                        ('Cancelled', 'Cancelled'),
-                        ('Inactive', 'Inactive')],
-                validators=[InputRequired()])
+    status = StringField()
     normalPrice = DecimalField('Enter normal ticket price', [InputRequired()])
     vipPrice = DecimalField('Enter VIP ticket price', [InputRequired()])
     normalAvail = IntegerField('Enter number of normal tickets', [InputRequired()])
@@ -47,10 +46,14 @@ class CreateEvent(FlaskForm):
     submit = SubmitField('Submit')
 
 class EditEvent(FlaskForm):
+    def validate_eventdate(form, field):
+        print("Validating date:", field.data)
+        if field.data < date.today():
+            raise ValidationError('The date cannot be in the past.')
     title = StringField('Enter event name')
     artist = StringField('Enter artist/group name')
     image = FileField('Event Image', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
-    date = DateField('Enter the date of event')
+    date = DateField('Enter the date of event', validators=validate_eventdate)
     startTime = TimeField('Enter the starting time of event')
     location = StringField('Enter venue name')
     country = StringField('Enter country (Spelling must be correct)') 
@@ -73,6 +76,7 @@ class OrderForm(FlaskForm):
     ticType = SelectField('Select Ticket Type', validators = [InputRequired()], choices = [('normTicket', 'Normal Ticket (Price: $200)'), ('vipticket', 'VIP Ticket (Price $300)')])
     numTickets = StringField("How many tickets?", validators = [InputRequired()])
     submit = SubmitField('Submit')
+
 
 
 
